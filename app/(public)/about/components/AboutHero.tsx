3b4265318bfbee88964f7843cd8e4642287cce11
Muhammad Umair <umair@assetize.today>
2025-11-14 21:24:07 +0500

    About page design changes.

--- app/(public)/about/components/AboutHero.tsx
+++ app/(public)/about/components/AboutHero.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link';
 
 const AboutHero = () => {
   return (
-    <section className='sm:pb-[50px] pb-2'>
-      <div className='md:pt-[309px] sm:pt-[70px] pt-[100px] sm:pl-[21px] space-y-7 sm:text-left text-center'>
-        <h1 className='font-modernist sm:text-[57.48px] text-[41.21px] font-normal leading-[100%] tracking-[-1.5px] text-white'>
+    <section className='sm:pb-[43px] pb-2'>
+      <div className='md:pt-[309px] sm:pt-[70px] pt-[100px] space-y-[26px] sm:text-left text-center'>
+        <h1 className='font-modernist sm:text-[57px] text-[41.21px] font-normal leading-[100%] tracking-[-1.5px] text-white'>
           About <br className='sm:hidden' />
           <span className='gradient-text'>Gami Vaults</span>
         </h1>
@@ -15,10 +15,10 @@ const AboutHero = () => {
           risk management
         </p>
 
-        <div className='flex justify-center items-center pt-1 sm:pt-2 sm:justify-start'>
+        <div className='flex justify-center items-center sm:justify-start'>
           <Link
             href='/vaults'
-            className='sm:px-[28.44px] px-2.5 sm:h-[40.89px] h-[30px] sm:rounded-[10px] rounded-[4px] bg-gradient-purple text-white sm:text-[14.22px] text-[10.2px] font-medium font-dm-sans hover:opacity-90 transition-opacity flex items-center justify-center'
+            className='sm:px-[28.44px] px-2.5 sm:h-[40.89px] h-[30px] sm:rounded-[10px] rounded-[4px] shadow-[0_0_0_0.4px_#ffffff47] bg-[#FFFFFF0F] text-white sm:text-[14.22px] text-[10.2px] font-medium font-dm-sans transition-all flex items-center justify-center hover:bg-gradient-purple'
           >
             Explore Vaults →
           </Link>
